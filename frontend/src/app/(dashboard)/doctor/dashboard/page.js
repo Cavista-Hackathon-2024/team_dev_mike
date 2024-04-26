c54f585f3b803f87ace54c77c4878268f8928c74
NureniJamiu <nurenijamiu951@gmail.com>
2024-04-26 14:35:17 +0100

    create project routes

---
+++ frontend/src/app/(dashboard)/doctor/dashboard/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Doctor's Dashboard</div>;
+};
+
+export default Page;
---
+++ frontend/src/app/(dashboard)/patient/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Patient's Dashboard</div>;
+};
+
+export default Page;
