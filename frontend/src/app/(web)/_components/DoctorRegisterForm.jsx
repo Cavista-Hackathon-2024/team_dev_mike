3ffc1e1dffbca454134af6c31a6eec45914cc135
NureniJamiu <nurenijamiu951@gmail.com>
2024-04-26 23:42:44 +0100

    fix: cors api error

--- frontend/src/app/(web)/_components/DoctorRegisterForm.jsx
+++ frontend/src/app/(web)/_components/DoctorRegisterForm.jsx
@@ -87,7 +87,6 @@ const DoctorRegisterForm = () => {
   };
 
   return (
-<<<<<<< HEAD
     <Form {...form}>
       <form onSubmit={form.handleSubmit(registerDoctor)}>
         <Card className="w-full max-w-xl">
@@ -210,64 +209,6 @@ const DoctorRegisterForm = () => {
         </Card>
       </form>
     </Form>
-=======
-    <Card className="w-full max-w-xl">
-      <CardHeader>
-        <CardTitle className="text-2xl">Register Doctor</CardTitle>
-        <CardDescription>
-          Enter your correct details below to register as a doctor.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-2">
-        <div>
-          <Label htmlFor="id">Doctor's ID</Label>
-          <Input
-            id="id"
-            type="text"
-            placeholder="DOC-6782867"
-            required
-            className="w-1/3"
-          />
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="firstname">First name</Label>
-            <Input
-              id="firstname"
-              type="text"
-              placeholder="firstname"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" type="text" placeholder="lastname" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email Address</Label>
-            <Input id="email" type="email" placeholder="email" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="phone">Phone number</Label>
-            <Input id="phone" type="text" placeholder="phone number" required />
-          </div>
-
-          <div className="grid gap-2">
-            <Label htmlFor="confirm_password">Confirm Password</Label>
-            <Input id="confirm_password" type="password" required />
-          </div>
-
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full bg-teal-600 hover:bg-teal-700">Sign in</Button>
-      </CardFooter>
-    </Card>
->>>>>>> 7c88bc31fad68befe30333fb46a353a8b0986c6b
   );
 };
 
