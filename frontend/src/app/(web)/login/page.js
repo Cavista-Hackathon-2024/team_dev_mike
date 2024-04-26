67b99226421b1d8f2fe64657d80b8b39a740b309
NureniJamiu <nurenijamiu951@gmail.com>
2024-04-26 15:35:41 +0100

    create login page

--- frontend/src/app/(web)/login/page.js
+++ frontend/src/app/(web)/login/page.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { LoginForm } from "../_components/LoginForm";
+import LoginImage from "../../../../public/assets/login.jpg";
+import Image from "next/image";
 
 const Page = () => {
-  return <div>Login</div>;
+  return (
+    <section className="flex h-[calc(100vh-65px)] items-center justify-center max-w-6xl mx-auto">
+      <LoginForm />
+    </section>
+  );
 };
 
 export default Page;
