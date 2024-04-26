a490b231c7bda702a97a0914bdb3997b5591cfcd
NureniJamiu <nurenijamiu951@gmail.com>
2024-04-26 15:04:46 +0100

    complete landing page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./(web)/_components/Navbar";
+import Footer from "./(web)/_components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,37 @@
 import Image from "next/image";
+import HeroImage from "../../public/assets/login.jpg";
 
 export default function Home() {
   return (
-    <main>
-      <p>Hello World</p>
+    <main className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 h-screen items-center">
+      <div className="  p-8 md:p-12 lg:px-16 lg:py-2">
+        <div className="flex flex-col mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+          <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
+            Prescription Precision: Making Healthcare Effortless
+          </h2>
+
+          <p className="hidden text-gray-500 md:mt-4 md:block">
+            Our system simplifies the process of managing prescriptions and
+            doctor's notes, providing a user-friendly platform tailored to the
+            needs of healthcare professionals.
+          </p>
+
+          <div className="mt-8 md:mt-16">
+            <a
+              href="#"
+              className="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
+            >
+              Get Started Today
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <Image
+        alt="Home page image"
+        src={HeroImage}
+        className="h-56 w-full object-cover sm:h-full"
+      />
     </main>
   );
 }
