ea74a448bfd92e9f11f1829e63731e6b3a2ab8c4
NureniJamiu <nurenijamiu951@gmail.com>
2024-04-27 03:47:55 +0100

    doctor's dashboard

--- frontend/src/app/(dashboard)/_components/DashboardTable.jsx
+++ frontend/src/app/(dashboard)/_components/DashboardTable.jsx
@@ -15,12 +15,32 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 const DashboardTable = () => {
   return (
     <Card>
-      <CardHeader className="px-7">
-        <CardTitle className="text-teal-700">Prescriptions</CardTitle>
-        <CardDescription>View recent prescriptions.</CardDescription>
+      <CardHeader className="px-7 w-full">
+        <div className="flex justify-between items-center w-full">
+          <div>
+            <CardTitle className="text-teal-700">Prescriptions</CardTitle>
+            <CardDescription>View recent prescriptions.</CardDescription>
+          </div>
+          <div>
+            <Button>Add new prescription</Button>
+          </div>
+        </div>
       </CardHeader>
       <CardContent>
         <Table>
@@ -43,16 +63,76 @@ const DashboardTable = () => {
                   liam@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell text-teal-600 cursor-pointer">
-                View
-              </TableCell>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <TableCell className="hidden sm:table-cell text-teal-600 cursor-pointer">
+                    View
+                  </TableCell>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px] md:max-w-[825px]">
+                  <DialogHeader>
+                    <DialogTitle>Patient's Prescription</DialogTitle>
+                    <DialogDescription>
+                      View prescriptions prescribed to patient
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid grid-cols-2 gap-8 p-3">
+                    <div className="bg-zinc-100 p-2 rounded-lg">
+                      <span className="font-semibold text-teal-600">
+                        Medication:
+                      </span>
+                      <p>Acetaminophen (Paracetamol): 500mg tablets</p>
+                      <ul className="list-disc">
+                        <span className="font-semibold text-teal-600">
+                          Instruction for use:
+                        </span>
+                        <li>
+                          Take 1 tablet every 4-6 hours as needed for headache
+                          relief. Do not exceed 3,000mg in 24 hours.
+                        </li>
+                        <li>Take the medication with a full glass of water.</li>
+                        <li>
+                          Avoid taking other over-the-counter pain relievers
+                          without consulting a healthcare professional.
+                        </li>
+                        <li>
+                          If headache persists or worsens after taking
+                          medication, seek medical attention.
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="bg-teal-50 p-2 rounded-lg">
+                      <ul className="list-disc">
+                        <span className="font-semibold text-teal-600">
+                          Additional Recomendations:
+                        </span>
+                        <li>Get plenty of rest </li>
+                        <li>Stay hydrated by drinking water regularly.</li>
+                        <li>
+                          Avoid triggers such as loud noise, bright lights, or
+                          strong smells.
+                        </li>
+                        <li>
+                          Practice relaxation techniques like deep breathing or
+                          meditation.
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </DialogContent>
+              </Dialog>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="secondary">
-                  Fulfilled
+                <Badge
+                  className="text-xs bg-red-100 text-red-500"
+                  variant="secondary"
+                >
+                  Used
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
-              <TableCell className="text-right">View Profile</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
@@ -65,12 +145,17 @@ const DashboardTable = () => {
                 View
               </TableCell>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="outline">
-                  Declined
+                <Badge
+                  className="text-xs bg-teal-100 text-teal-500"
+                  variant="outline"
+                >
+                  Unused
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
-              <TableCell className="text-right">$150.00</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
@@ -83,12 +168,17 @@ const DashboardTable = () => {
                 View
               </TableCell>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="secondary">
-                  Fulfilled
+                <Badge
+                  className="text-xs bg-red-100 text-red-500"
+                  variant="secondary"
+                >
+                  Used
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-25</TableCell>
-              <TableCell className="text-right">$350.00</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
@@ -101,12 +191,17 @@ const DashboardTable = () => {
                 View
               </TableCell>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="secondary">
-                  Fulfilled
+                <Badge
+                  className="text-xs bg-red-100 text-red-500"
+                  variant="secondary"
+                >
+                  Used
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
-              <TableCell className="text-right">$450.00</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
@@ -119,12 +214,17 @@ const DashboardTable = () => {
                 View
               </TableCell>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="secondary">
-                  Fulfilled
+                <Badge
+                  className="text-xs bg-red-100 text-red-500"
+                  variant="secondary"
+                >
+                  Used
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
-              <TableCell className="text-right">View Profile</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
@@ -137,12 +237,17 @@ const DashboardTable = () => {
                 View
               </TableCell>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="secondary">
-                  Fulfilled
+                <Badge
+                  className="text-xs bg-red-100 text-red-500"
+                  variant="secondary"
+                >
+                  Used
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
-              <TableCell className="text-right">View Profile</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
@@ -155,12 +260,17 @@ const DashboardTable = () => {
                 View
               </TableCell>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="outline">
-                  Declined
+                <Badge
+                  className="text-xs bg-teal-100 text-teal-500"
+                  variant="outline"
+                >
+                  Unused
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
-              <TableCell className="text-right">$150.00</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
@@ -173,12 +283,17 @@ const DashboardTable = () => {
                 View
               </TableCell>
               <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs" variant="secondary">
-                  Fulfilled
+                <Badge
+                  className="text-xs bg-red-100 text-red-500"
+                  variant="secondary"
+                >
+                  Used
                 </Badge>
               </TableCell>
               <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
-              <TableCell className="text-right">$450.00</TableCell>
+              <TableCell className="text-right text-teal-700">
+                View profile
+              </TableCell>
             </TableRow>
           </TableBody>
         </Table>
--- frontend/src/app/(dashboard)/layout.jsx
+++ frontend/src/app/(dashboard)/layout.jsx
@@ -1,7 +1,165 @@
-import React from "react";
+import Link from "next/link";
+import {
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  LucideBriefcaseMedical,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+  UsersIcon,
+} from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/">
+              <span className="text-teal-600">
+                MedVault<span className="font-bold">Connect</span>
+              </span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="/doctor/dashboard"
+                className="flex items-center gap-3 bg-teal-100 rounded-lg px-3 py-2 text-teal-800 transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Dashboard
+              </Link>
+              <Link
+                href="/doctor/prescriptions"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LucideBriefcaseMedical className="h-4 w-4" />
+                Prescriptions
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 bg-teal-800 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="/doctor/patients"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <UsersIcon className="h-4 w-4" />
+                Patients
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <span className=" text-teal-600">
+                    MedVault<span className="font-bold">Connect</span>
+                  </span>
+                  <span className="sr-only text-teal-600">
+                    MedVault<span className="font-bold">Connect</span>
+                  </span>
+                </Link>
+                <Link
+                  href="/doctor/dashboard"
+                  className="mx-[-0.65rem] flex items-center bg-teal-100 gap-4 rounded-xl px-3 py-2 text-teal-800 hover:text-foreground"
+                >
+                  <Home className="h-5 w-5" />
+                  Dashboard
+                </Link>
+                <Link
+                  href="/doctor/prescriptions"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-foreground hover:text-foreground"
+                >
+                  <ShoppingCart className="h-5 w-5" />
+                  Prescriptions
+                  <Badge className="ml-auto bg-teal-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                    6
+                  </Badge>
+                </Link>
+                <Link
+                  href="/doctor/patients"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Package className="h-5 w-5" />
+                  Patients
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <div className="w-full flex-1">
+            <form>
+              <div className="relative">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search patients..."
+                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                />
+              </div>
+            </form>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+
+        <main className="p-6">{children}</main>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
